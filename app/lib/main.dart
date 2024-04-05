7c32b6cca612c31e24a6b702927dd4147ef890d1
Matteo Boetti <boettimatteo97@gmail.com>
2024-04-05 16:18:54 +0200

    initial commit

---
+++ app/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:products_app/app/app.dart';
+import 'package:products_app/bootstrap.dart';
+
+/// only used by native run
+void main() {
+  bootstrap(() => const App());
+}
---
+++ app/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:products_app/app/app.dart';
+import 'package:products_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
